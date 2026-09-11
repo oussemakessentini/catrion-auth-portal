6ddfd039ce040d8b3a0b2906a33f541272aa5b76
oussemakessentini <oussamaksantini@gmail.com>
2026-09-10 23:21:52 -0500

    feat: initialize Spring Boot backend

---
+++ auth-portal/src/test/java/com/catrion/auth_portal/AuthPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catrion.auth_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
